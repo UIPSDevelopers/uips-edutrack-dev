5732f1a848b8b9ce83ec9ff58b8ff370f760824b
JC <jctanmaquera@gmail.com>
2026-05-07 11:53:38 +0400

    fix axiosInstance URL

--- src/pages/PropertyTagging/BulkImportAssets.jsx
+++ src/pages/PropertyTagging/BulkImportAssets.jsx
@@ -177,7 +177,7 @@ export default function BulkImportAssets() {
         locationId: r.locationId,
       }));
 
-      const { data } = await axiosInstance.post("/assets/bulk-import", {
+      const { data } = await axiosInstance.post("/assets/bulk-create", {
         assets: payload,
       });
 
